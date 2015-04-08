e6a101e3974e0102569cc3640893244e9f21f0b7
Mateusz Tuński <mateusz@tunski.co>
2015-04-08 16:06:02 +0200

    Add `currencies` table and empty Currency model:
    - use uuid as the PK in `currencies` table

---
+++ db/migrate/20150408140013_create_currencies.rb
@@ -0,0 +1,12 @@
+class CreateCurrencies < ActiveRecord::Migration
+  def change
+    create_table :currencies, id: :uuid do |t|
+      t.text    :rates
+      t.boolean :consumer1_ack, default: false
+      t.boolean :consumer2_ack, default: false
+      t.boolean :consumer3_ack, default: false
+
+      t.timestamps
+    end
+  end
+end
