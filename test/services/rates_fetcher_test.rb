218c79e577bc61b3adf4c5810c183ebc200535de
Mateusz Tuński <mateusz@tunski.co>
2015-04-24 17:35:13 +0200

    Export OpenExchange stub to separate module and put it in test_helper

--- test/services/rates_fetcher_test.rb
+++ test/services/rates_fetcher_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class RatesFetcherTest < ActiveSupport::TestCase
+  include OpenExchangeStub
+
   setup do
     @rates_fetcher = RatesFetcher.new
-    stub_openexchange_api_request
+    @response_mock = stub_openexchange_api_request
   end
 
   def test_fetches_and_saves_rates_if_none_present_in_the_database
@@ -24,22 +26,5 @@ class RatesFetcherTest < ActiveSupport::TestCase
 
   def test_returns_latest_rates_from_the_database_if_not_older_than_1_hour
     assert_equal Currency.create!, @rates_fetcher.call
-  end
-
-  private
-
-  def stub_openexchange_api_request
-    @response_mock = {
-      'rates' => {
-        'PLN' => 3.729895,
-        'EUR' => 0.928687,
-        'CHF' => 0.968524
-      }
-    }
-
-    stub_request(:get, RatesFetcher::OPENEXCHANGE_API_URL).to_return(
-      status: 200,
-      body:   @response_mock.to_json
-    )
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,31 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'webmock/minitest'
 
 class ActiveSupport::TestCase
   fixtures :all
+end
+
+module OpenExchangeStub
+  require 'webmock/minitest'
+
+  private
+
+  def stub_openexchange_api_request
+    response_mock = {
+      'rates' => {
+        'PLN' => 3.729895,
+        'EUR' => 0.928687,
+        'CHF' => 0.968524
+      }
+    }
+
+    stub_request(:get, RatesFetcher::OPENEXCHANGE_API_URL).to_return(
+      status: 200,
+      body:   response_mock.to_json
+    )
+
+    response_mock
+  end
 end
